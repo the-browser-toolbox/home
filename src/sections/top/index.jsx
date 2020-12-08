7a4d875636ca4fe6ff233ba20243be7b3f7ec1ed
Abir <abir.pal899@gmail.com>
2020-12-09 00:54:24 +0530

    Added Packages

--- src/sections/top/index.jsx
+++ src/sections/top/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { Typography, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/";
+import { motion } from "framer-motion";
 const Top = ()=>{
 <>
 
