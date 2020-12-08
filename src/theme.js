e0a80c8f0c9c308ce7b63109de644f2418d73389
Abir <abir.pal899@gmail.com>
2020-12-09 00:56:53 +0530

    Added Theme

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from "@material-ui/core/";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Segoe UI",
+    h2: {
+      fontSize: "29px"
+    },
+    h3: {
+      fontSize: "24px",
+      fontFamily: "Segoe UI"
+    },
+    h4: {
+      fontSize: "22px",
+      fontFamily: "Segoe UI",
+      fontWeight: "700"
+    },
+    body1: {
+      fontSize: "18px",
+      fontFamily: "Segoe UI",
+      fontWeight: "600"
+    },
+    body2: {
+      fontFamily: "Raleway",
+      fontWeight: "500",
+      fontSize: "19px"
+    }
+  }
+});
+
+export default theme;
