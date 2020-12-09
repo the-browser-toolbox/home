55822cb51c494462bba36c8c2937e3386a7dbba4
Abir <abir.pal899@gmail.com>
2020-12-09 11:13:06 +0530

    Updated Links

--- src/sections/bottom/index.jsx
+++ src/sections/bottom/index.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import { Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/";
 import { motion } from "framer-motion";
-const Bottom = ()=>{
-<>
 
-</>
-}
+const useStyles= makeStyles((theme)=>({
+    root:{
+    
+    },
+    imagePadding:{
+  
+    }   
+
+}))
+
+const Bottom = () => {
+    const classes = useStyles();
+    return (
+    <motion.div className={classes.root}>
+     
+        <Grid container>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+                <Typography gutterBottom={true} textAlign="center" variant="h2">
+                   Extensions
+                </Typography>
+            </Grid>
+        </Grid>
+    
+    </motion.div>
+)}
 export default Bottom;
