b29a7feb455b90b3a9e7b93603e510d23c283332
Abir <abir.pal899@gmail.com>
2020-12-09 11:08:21 +0530

    Middlel Section Updated

--- src/sections/middle/index.jsx
+++ src/sections/middle/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Typography, Grid, Container, Button } from "@material-ui/core";
+import { Typography, Grid, Container} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/";
 import { motion } from "framer-motion";
+import BTN from '../../components/buttons'
 const useStyles = makeStyles((theme) => ({
     root: {
         background: '#252323',
         color: 'white',
         paddingTop: '25px',
         paddingBottom: '125px',
-        paddingLeft:"4px"
+        paddingLeft: "4px"
     },
     headPadding: {
         marginBottom: '32px'
@@ -36,32 +37,25 @@ const Middle = () => {
                                 Browser extensions are very powerful tools to enhance
                               the browsing experience.<br />We at <b> “The Browser ToolBox” </b>,
                              build super cool Open Source Browser Extensions to make lives easier.
-                             <br /><br/>
+                             <br /><br />
                             </Typography>
                         </Grid>
                         <Grid container spacing={4}>
                             <Grid classname={classes.buttonMargin} item xs={12} sm={12} md={3} lg={3} >
-                                <Button
-                                    href="https://github.com/the-browser-toolbox"
-                                    variant="outlined"
-                                    target="_blank"
+                                <BTN
+                                    name="Contribute on GitHub"
+                                    link="https://github.com/the-browser-toolbox"
                                     color="primary"
-                                    rel="noopener noreferrer"
-
-                                >
-                                    Contribute on GitHub
-                                </Button>
+                                />
                             </Grid>
                             <Grid classname={classes.buttonMargin} item xs={12} sm={12} md={9} lg={9} >
-                                <Button
-                                    href="https://gitter.im/the-browser-toolbox/community"
-                                    variant="outlined"
-                                    target="_blank"
+                                <BTN
+                                    link="https://gitter.im/the-browser-toolbox/community"
+                                    name=" Join the Channel"
                                     color="secondary"
-                                    rel="noopener noreferrer"
-                                >
-                                    Join the Channel
-                            </Button>
+                                />
+
+
                             </Grid>
                         </Grid>
 
